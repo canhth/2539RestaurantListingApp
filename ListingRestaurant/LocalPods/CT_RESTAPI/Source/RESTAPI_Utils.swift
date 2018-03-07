5d00caa16e477fd7690e991b80b85773eb98ef1e
Tran Hoang Canh <canh.tran@ascendcorp.com>
2018-03-07 10:33:57 +0700

    Remove something unused

--- ListingRestaurant/LocalPods/CT_RESTAPI/Source/RESTAPI_Utils.swift
+++ ListingRestaurant/LocalPods/CT_RESTAPI/Source/RESTAPI_Utils.swift
@@ -16,24 +16,6 @@ public protocol CTArrayType {
 extension Array: CTArrayType {}
 
 
-public extension UIImage {
-    class func getImageProfileURL(fileName: String) -> URL {
-        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-        let documentsURL = paths[0]
-        let filePath = documentsURL.appendingPathComponent(fileName)
-        return filePath
-    }
-    
-    class func getImageProfile(fileName: String) -> UIImage {
-        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-        let documentsURL = paths[0]
-        let filePath = documentsURL.appendingPathComponent(fileName)
-        let image  = UIImage(contentsOfFile: filePath.path)
-        return image ?? UIImage(named: "ic_avatar")!
-    }
-}
-
-
 public extension Observable {
     public func continueWithSuccessClosure(block: @escaping (_ element: Element) -> Observable<Element>) -> Observable<Element> {
         return self.flatMapLatest({ (element) -> Observable<Element> in
